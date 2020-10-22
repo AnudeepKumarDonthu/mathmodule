b887ea98676a61914f4a7fd8835812817505178b
Anudeep Kumar <donthuanudeepkumar@gmail.com>
2020-10-22 22:46:46 +0530

    Zero Value is not accepted

--- geometry/cube.go
+++ geometry/cube.go
@@ -1,6 +1,13 @@
 package geometry
 
+import "errors"
+
 // Cubevolueme function is used to Find the Volume of a Ccube
-func Cubevolueme(n int) int {
-	return n * n * n
+func Cubevolueme(n int) (int, error) {
+
+	if n != 0 {
+		return n * n * n, nil
+	}
+	return 0, errors.New("Cube with side size 0 is not allowed")
+
 }
